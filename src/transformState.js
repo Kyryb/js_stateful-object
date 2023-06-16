b884a6155ab47883c7335a819299c1f13e0010d2
Kyryb <kyrybcode@gmail.com>
2023-06-16 21:07:14 +0300

    Task solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,39 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        addProperties(state, extraData);
+        break;
+      case 'clear':
+        clearObject(state);
+        break;
+      case 'removeProperties':
+        removeProperties(state, keysToRemove);
+        break;
+      default:
+        return 'Uknown property was found';
+    }
+  }
+}
+
+function addProperties(target, properties) {
+  Object.assign(target, properties);
+}
+
+function clearObject(target) {
+  for (const key in target) {
+    delete target[key];
+  }
+}
+
+function removeProperties(target, properties) {
+  for (const key of properties) {
+    delete target[key];
+  }
 }
 
 module.exports = transformState;
